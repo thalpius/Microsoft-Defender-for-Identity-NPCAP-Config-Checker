bf50b4c916e18bd8eb2d0c1e556664c7c46ac805
thalpius <49958238+thalpius@users.noreply.github.com>
2024-07-27 12:22:32 +0200

    First commit

---
+++ PowerShell/MDI-NPCAP-Config-Checker.ps1
@@ -0,0 +1,153 @@
+<#
+ 
+.SYNOPSIS
+  This script check if NPCAP is configured correctly for Microsoft Defender for Identity
+ 
+.DESCRIPTION
+ 
+  Microsoft Defender for Identity uses NPCAP to inspect packets for malicious intent.
+  Sometimes, NPCAP is not configured correctly for Microsoft Defender for Identity or
+  NPCAP is installed by another program with different settings resulting in health
+  issues reported by Microsoft Defender for Identity. This script will check the NPCAP
+  settings which Microsoft Defender for Identity expects. If one of the items fail, validate
+  if the setting can be changed to whatever it expects and restart the NPCAP service.
+ 
+.EXAMPLES
+
+  MDIcheckServices
+  MDIcheckRegistryProperty -registryKey 'HKLM:\\SOFTWARE\\Wow6432Node\\npcap' -registryProperty 'AdminOnly' -registryValue 0
+  MDIcheckRegistryKey -registryKey 'HKLM\\SYSTEM\\CurrentControlSet\\Services\\npcap\\Parameters' -registryProperty 'LoopbackAdapter'
+  
+.INPUTS
+ 
+  None
+ 
+.OUTPUTS
+ 
+  Output will be shown in the terminal/console.
+ 
+.NOTES
+ 
+  Version:        0.1
+  Author:         R. Roethof
+  Creation Date:  07/27/2024
+  Website:        https://thalpius.com
+  Purpose/Change: Initial script development
+
+#>
+
+#------------------------------------------[Initialisations]---------------------------------------
+ 
+# Set Error Action to Silently Continue
+$ErrorActionPreference = "SilentlyContinue"
+
+#-------------------------------------------[Declarations]-----------------------------------------
+
+# Variables that can be set
+
+
+#--------------------------------------------[Functions]-------------------------------------------
+
+function MDIcheckServices {
+    Param(
+    )
+    begin {
+    }
+    process {
+        try {
+            if ((Get-Service -Name npcap -ErrorAction SilentlyContinue) -or (Get-Service -Name npf -ErrorAction SilentlyContinue)) {
+                Write-Host 'Checking if NPCAP service or NPF service is running: ' -NoNewline
+                Write-Host 'Pass' -ForegroundColor Green
+            }
+            else {
+                Write-Host 'Checking if NPCAP service or NPF service is not running: ' -NoNewline
+                Write-Host 'Fail' -ForegroundColor Red
+            }
+        }
+        catch {
+            Write-Host $_.Exception
+            exit
+        }
+    }
+    end {
+        if ($?) {
+        }
+    }
+}
+
+function MDIcheckRegistryProperty {
+    Param(
+    [parameter(Mandatory = $true, HelpMessage = "Specify the registry key")]
+    [ValidateNotNullOrEmpty()]
+    [string]$registryKey,
+    [parameter(Mandatory = $true, HelpMessage = "Specify the registry property")]
+    [ValidateNotNullOrEmpty()]
+    [string]$registryProperty,
+    [parameter(Mandatory = $true, HelpMessage = "Specify the registry value")]
+    [ValidateNotNullOrEmpty()]
+    [string]$registryValue
+    )
+    begin {
+    }
+    process {
+        try {
+            if ((Get-ItemProperty -Path $registryKey).$registryProperty -eq $registryValue) {
+                Write-Host "Checking if property $registryProperty in $registryKey has the value"$registryValue": " -NoNewline
+                Write-Host "Pass" -ForegroundColor Green
+            }
+            else {
+                Write-Host "Checking if property $registryProperty in $registryKey has the value"$registryValue": " -NoNewline
+                Write-Host "Fail" -ForegroundColor Red
+            }
+        }
+        catch {
+            Write-Host $_.Exception
+            exit
+        }
+    }
+    end {
+        if ($?) {
+        }
+    }
+}
+
+function MDIcheckRegistryKey {
+    Param(
+    [parameter(Mandatory = $true, HelpMessage = "Specify the registry key")]
+    [ValidateNotNullOrEmpty()]
+    [string]$registryKey,
+    [parameter(Mandatory = $true, HelpMessage = "Specify the registry property")]
+    [ValidateNotNullOrEmpty()]
+    [string]$registryProperty
+    )
+    begin {
+    }
+    process {
+        try {
+            if ((Get-ItemProperty -Path $registryKey).$registryProperty) {
+                Write-Host "Checking if property $registryProperty in $registryKey doesn't exist: " -NoNewline
+                Write-Host "Fail" -ForegroundColor Red
+            }
+            else {
+                Write-Host "Checking if property $registryProperty in $registryKey doesn't exist: " -NoNewline
+                Write-Host "Pass" -ForegroundColor Green
+            }
+        }
+        catch {
+            Write-Host $_.Exception
+            exit
+        }
+    }
+    end {
+        if ($?) {
+        }
+    }
+}
+
+MDIcheckServices
+MDIcheckRegistryProperty -registryKey 'HKLM:\\SOFTWARE\\Wow6432Node\\npcap' -registryProperty 'AdminOnly' -registryValue 0
+MDIcheckRegistryProperty -registryKey 'HKLM:\\SOFTWARE\\Wow6432Node\\npcap' -registryProperty 'WinPcapCompatible' -registryValue 1
+MDIcheckRegistryProperty -registryKey 'HKLM:\\SYSTEM\\CurrentControlSet\\Services\\npcap\\Parameters' -registryProperty 'AdminOnly' -registryValue 0
+MDIcheckRegistryProperty -registryKey 'HKLM:\\SYSTEM\\CurrentControlSet\\Services\\npcap\\Parameters' -registryProperty 'LoopbackSupport' -registryValue 1
+MDIcheckRegistryProperty -registryKey 'HKLM:\\SYSTEM\\CurrentControlSet\\Services\\npcap\\Parameters' -registryProperty 'WinPcapCompatible' -registryValue 1
+MDIcheckRegistryKey -registryKey 'HKLM\\SYSTEM\\CurrentControlSet\\Services\\npcap\\Parameters' -registryProperty 'LoopbackAdapter'
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Microsoft-Defender-for-Identity-NPCAP-Config-Checker
+> [!IMPORTANT]
+> "Blog post is coming ASAP"
+
+# Microsoft Defender for Identity NPCAP Config Checker
+
+I created a small program to test the "Entities exposing credentials in clear text" Secure Score Recommended Action for Microsoft Defender for Identity. For more information, please check the official documentation from Microsoft:
+
+https://learn.microsoft.com/en-us/defender-for-identity/health-alerts#sensor-has-issues-with-packet-capturing-component
+
+## ⚙️ Features
+
+- Creates an LDAP connection to a Domain Controller using a simple Bind
+- Using arguments for the server, username, and password to make it simple for anyone to use
+- Triggers "Exposed Entities" for the "Stop clear text credentials exposure" recommended action in secure score
+
+## 🛠️ Usage
+
+Enter a Domain Controller as the ldapserver, the Distinguished Name of a user account, and the corresponding password. Here is an example:
+````
+SimpleLDAPBind.exe /ldapServer:"DC01" /userDN:"CN=thalpius,CN=Users,DC=yoshis,DC=island" /password:"SimpleLDAPBind01!"
+````
+
+> **_NOTE:_** Use the "Attribute Editor" in "Active Directory Users and Computers" on the object to find the Distinguished Name for a user account.
+
+## 📄 Blog
+
+To find out more about configuring NPCAP for Microsoft Defender for Identity, check out my blog post:
+
+https://thalpius.com/
+
+## 📺 Screenshots
+Here is an example of a passed config for NPCAP:
+
+![Alt text](/Screenshots/MicrosoftDefenderForIdentitySimpleLDAPBind01.png?raw=true "Microsoft Defender for Identity NPCAP Config Checker Pass")
+
+Here is an example of a failed config for NPCAP:
+
+![Alt text](/Screenshots/MicrosoftDefenderForIdentitySimpleLDAPBind02.png?raw=true "Microsoft Defender for Identity NPCAP Config Checker Fail")
